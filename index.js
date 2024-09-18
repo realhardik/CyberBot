3ab1952e8690e923752d4bf49ee35f876b78c942
dev-hail <hardik.dev.08@gmail.com>
2024-09-18 12:24:14 +0530

    OTP verification

--- index.js
+++ index.js
@@ -47,9 +47,8 @@ const h = {
 
 class cyberBot {
     constructor() {
-        this.client = new Client();
+        this.client = new Client()
         this.client.initialize()
-        this.categories = categories[0].opt
         h.bind(this, ['generateQR', 'receiveMessage', 'sendMessage', 'storeMessages'])
         this.generateQR()
     }
@@ -66,7 +65,6 @@ class cyberBot {
 
             if (connected) {
                 console.log('Chatbot is live!')
-                this.receiveMessage()
             }
         });
     }
@@ -81,80 +79,26 @@ class cyberBot {
                 //     contact: this.senderNumber
                 // }, 'user_ref', 'findOne')
     
-                // this.storeMessages(message, sender)
+                // this.storeMessages(sender, message)
                 
-                if (t) {
-                    resolve(message.body)
-                    return
-                }
-                
-                if (message in this.categories) {
-                    this.sendMessage(message, message.from)
-                } else if (null == user) {
-                    var reply = `Hi, \nBefore we get started, we\'d like to gather a few details to better assist you.\nCould you please provide the following information?`
-                    await this.client.sendMessage(message.from, reply)
-                    this.sendMessage('starters', message.from)
-                } else {
-                    var reply = `Hello ${user}! How can I help you today?`
-                    await this.client.sendMessage(message.from, reply)
-                    this.sendMessage('categories', message.from)
-                }
-                resolve(message.body)
+                resolve({
+                    message: message,
+                    sender: message.from,
+                    body: message.body.toLowerCase(),
+                    user: user || null
+                })
             })
         })
         
     }
 
-    async sendMessage(type, sender) {
-        const qPath = path.join(__dirname, 'Questions', `${type}.json`)
+    async sendMessage(sender, message) {
         try {
-            const questions = require(qPath)
-            let response;
-
-            for (var c = 0; c<questions.length; c++) {
-                var question = questions[c].q,
-                    opt = questions[c].opt
-                console.log(question)
-                console.log(opt)
-
-                if (!opt || opt.length === 0) {
-                    var message = question
-                } else {
-                    var options = Array.from({ length: opt.length }, (v, i) => {
-                        return `${i+1}. ${opt[i]}`
-                    }).join('\n')
-                    message = question + '\n' + options
-                    console.log(message)
-                }
-
-                await this.client.sendMessage(sender, message);
-
-                if (question.includes("E-mail for verification")) {
-                    response = await this.receiveMessage('verify')
-                    response = response.toLowerCase()
-                    var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                        format = response.match(regex),
-                        attempts = 0;
-
-                    while (!format) {
-                        if (attempts>2) {
-                            await this.client.sendMessage(sender, 'Failed to validate email.')
-                            this.receiveMessage()
-                            break
-                        }
-                        format = response.match(regex)
-                        var message = "Please provide valid email"
-                        await this.client.sendMessage(sender, message)
-                        response = await this.receiveMessage('verify')
-                        attempts++
-                    }
-                    this.receiveMessage(null, "abc")
-                } else {
-                    response = await this.receiveMessage('data')
-                }
-            }
+            // this.storeMessages('chatbot', message)
+            await this.client.sendMessage(sender, message);
+            
         } catch (err) {
-            console.error('Error with the chatbot. \nError: ', err)
+            console.error('Error sending the message. \nError: ', err)
         }
     }
 
@@ -163,8 +107,149 @@ class cyberBot {
     }
 }
 
+class Flow {
+    constructor() {
+        this.bot = new cyberBot
+        this.categories = categories[0].opt
+        this.flow = ["greet", "categories", "essential", "related", "options"]
+        this.greet()
+    }
+
+    async greet() {
+        let res = await this.bot.receiveMessage(),
+            reply;
+        if ("hello" === res.body) {
+
+            if (res.user) {
+                reply = `Hello ${res.user}! How can I help you today?`
+                await this.bot.sendMessage(res.sender, reply)
+                this.categories()
+            } else if (null == res.user) {
+                reply = `Hi, \nBefore we get started, we\'d like to gather a few details to better assist you.\nCould you please provide the following information?`
+                await this.bot.sendMessage(res.sender, reply)
+                this.starters(res)
+            }
+
+        } else {
+            reply = "Type 'Hello' to start the conversation."
+            await this.bot.sendMessage(res.sender, reply)
+            this.greet()
+        }
+    }
+
+    async starters(res) {
+        let questions = require("./Questions/starters.json"),
+            response,
+            sender = res.sender,
+            user = {}
+
+        for (var c = 0; c<questions.length; c++) {
+            let question = questions[c].q,
+                opt = questions[c].opt;
+
+            if (!opt || opt.length === 0) {
+                question = question
+            } else {
+                var options = Array.from({ length: opt.length }, (v, i) => {
+                    return `${i+1}. ${opt[i]}`
+                }).join('\n')
+                question = question + '\n' + options
+            }
+
+            this.bot.sendMessage(sender, question)
+            response = await this.bot.receiveMessage()
+
+            if (question.includes("E-mail")) {
+                
+                var email = res.body.toLowerCase(),
+                    regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    format = email.match(regex),
+                    attempts = 0;
+
+                while (!format) {
+                    if (attempts>3) {
+                        await this.bot.sendMessage(sender, 'Failed to validate email.')
+                        this.greet()
+                        return
+                    }
+                    format = email.match(regex)
+                    var message = "Please provide valid email"
+                    await this.bot.sendMessage(sender, message)
+                    response = await this.bot.receiveMessage('verify')
+                    email = response.body.toLowerCase()
+                    attempts++
+                }
+
+                let verification = await this.verifyEmail(email, sender)
+                if (verification) {
+                    user[questions[c].id] = email
+                    // db.createUser(user)
+                    console.log(user)
+                    this.categories()
+                }
+            } else {
+                user[questions[c].id] = response.body
+            }
+        }
+    }
+
+    categories() {
+        
+    }
+
+    async verifyEmail(email, sender) {
+        const nodemailer = require('nodemailer'),
+            otpGen = require('otp-generator');
+        
+        let otp = otpGen.generate(6, { upperCaseAlphabets: false, specialChars: false }),
+            transporter = nodemailer.createTransport({
+                service: 'gmail', // e.g., Gmail
+                auth: {
+                    user: 'johnwickxh@gmail.com', // Your email
+                    pass: 'syky zwhr arud hpnt' // Your email password or app-specific password
+                }
+            })
+        
+        let mailOptions = {
+            from: '"CyberBot noreply" <your-email@gmail.com>', // sender address
+            to: email, // receiver's email
+            subject: 'Email Verification - OTP', // Subject
+            text: `Your OTP for verification is: ${otp}`, // Plain text body
+            html: `<h5>Your OTP for verification is: <b>${otp}</b></h5>` // HTML body
+        };
+
+        try {
+            let info = await transporter.sendMail(mailOptions);
+            await this.bot.sendMessage(sender, "OTP sent on given Email address.\nPlease Enter to proceed.")
+            let res = await this.bot.receiveMessage()
+            if (res.body == otp) {
+                await this.bot.sendMessage(sender, "Email verified successfully.")
+                return true
+            } else {
+                var attempts = 0
+                await this.bot.sendMessage(sender, "Invalid OTP. Please Try again.")
+                return false
+                // while (res != otp) {
+                //     if (attempts > 2) {
+                //         return false
+                //     }
+                //     
+                //     otp = otpGen.generate(6, { upperCaseAlphabets: false, specialChars: false })
+                //     info = await transporter.sendMail(mailOptions);
+                //     await this.bot.sendMessage(sender, "New OTP sent")
+                //     let res = await this.bot.receiveMessage()
+                // }
+            }
+
+        } catch (error) {
+            console.error('Error sending email:', error);
+        }
+
+    }
+}
+
 new class {
     constructor() {
-        new cyberBot
+        new Flow
     }
 }
