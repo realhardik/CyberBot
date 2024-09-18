edcdf0605cd2bcf789ca510043f8a1af5dbce032
dev-hail <hardik.dev.08@gmail.com>
2024-09-18 22:33:00 +0530

    OLA Api working, nearby police station location sent

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, MessageMedia, Buttons } = require('whatsapp-web.js');
+const { Client, MessageMedia, Buttons, Location } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 // const mongoose = require('mongoose');
 const path = require('path');
@@ -43,24 +43,32 @@ const h = {
         for (let t = 0; t < s; t++)
             r[e[t]] = r[e[t]].bind(r)
     },
-    options: (q, o, m) => {
+    options: (q, o) => {
         var options = Array.from({ length: o.length }, (v, i) => {
             return `${i+1}. ${o[i]}`
-        }).join('\n'),
-        temp = `Which questions would you like to answer?\n Provide Option no. separated by comma eg: 1, 2, 3`
-        q = q + "\n" + options + "\n" + (m ?  temp : `Provide Option no. [1-${o.length}]`)
+        }).join('\n')
+        q = q + "\n" + options + "\n" + `Provide Option no. [1-${o.length}]`
         return q
     },
-    getOption: (r, i) => {
+    getOption: (o, i) => {
         var input = i.split(',').map(option => option.trim()),
-            uOptions = [...new Set(input.map(Number))].filter(option => option > 0 && option <= r.length)
-            return  (uOptions.length > 0 ? r[uOptions[0]-1] : false)
+            uOptions = [...new Set(input.map(Number))].filter(option => option > 0 && option <= o.length)
+            return  (uOptions.length > 0 ? o[uOptions[0]-1] : false)
     },
     checkOptions: (i, o) => {
         var max = o,
-            input = i.split(',').map(option => option.trim()),
-            uOptions = [...new Set(input.map(Number))].filter(option => option > 0 && option <= max)
+            input = i.split(',').map(option => (parseInt(option.trim()) - 1)),
+            uOptions = [...new Set(input.map(Number))].filter(option => option >= 0 && option < max)
         return uOptions
+    },
+    multipleOptions: (q) => {
+        var starter = "From what we\'ve seen, here are some common questions that might be helpful. \nFeel free to pick the ones you\'d like to answer.",
+            options = Array.from({ length: q.length }, (v, i) => {
+                return `${i+1}. ${q[i].q}`
+            }).join('\n'),
+            end = `Provide Option no(s). separated by comma [1-${q.length}]\neg: 1, 2, 3`,
+            question = starter + "\n" + options + "\n" + end
+        return question
     }
 }
 
@@ -134,14 +142,14 @@ class Flow {
     }
 
     async greet() {
-        let res = await this.bot.receiveMessage(),
+        let res = await this.bot.receiveMessage(null, "abc"),
             reply;
         if ("hello" === res.body) {
 
             if (res.user) {
                 reply = `Hello ${res.user}! How can I help you today?`
                 await this.bot.sendMessage(res.sender, reply)
-                this.categories(res.sender)
+                this.essentials(res.sender)
             } else if (null == res.user) {
                 reply = `Hi, \nBefore we get started, we\'d like to gather a few details to better assist you.\nCould you please provide the following information?`
                 await this.bot.sendMessage(res.sender, reply)
@@ -167,9 +175,7 @@ class Flow {
                 opt = questions[c].opt,
                 oflag = !1;
 
-            if (!opt || opt.length === 0) {
-                question = question
-            } else {
+            if (opt.length > 0) {
                 oflag = !0
                 question = h.options(question, opt)
             }
@@ -205,16 +211,41 @@ class Flow {
                     user[questions[c].id] = email
                     // db.createUser(user)
                     console.log(user)
-                    this.categories(sender)
+                    this.essentials(sender)
+                    return
+                } else {
+                    this.greet()
+                    return
                 }
-            } else {
-                user[questions[c].id] = response
+            } else if (question.includes("age")) {
+                response = Number(response)
+                var attempts = 0
+                if (response) {
+                    while (response > 99 && response < 15) {
+                        if (attempts > 3)
+                            return (this.greet(), false)
+                        
+                            if (response<15) {
+                                await this.bot.sendMessage(sender, "Only 15+ age users are allowed to use this chatbot.")
+                            } else if (response>99) {
+                                await this.bot.sendMessage(sender, "Please provide a valid age.")
+                            }
+                        var i = await this.bot.receiveMessage()
+                        response = Number(i.body)
+                        attempts++
+                    }
+                } else {
+                    await this.bot.sendMessage(sender, "Please provide a valid age.")
+                    this.greet()
+                }
             }
+            user[questions[c].id] = response
         }
     }
 
-    async categories(sender) {
-        var eQuestions = require("./Questions/essentials.json")
+    async essentials(sender) {
+        var eQuestions = require("./Questions/essentials.json"),
+            res = {}
         
         for (var c=0; c<eQuestions.length; c++) {
             var q = eQuestions[c].q,
@@ -227,8 +258,132 @@ class Flow {
             }
             await this.bot.sendMessage(sender, q)
             var response = await this.bot.receiveMessage()
+            response = response.body
+            oflag && (response = h.getOption(opt, response))
             // ---
+            res[eQuestions[c].id] = response.toLowerCase()
         }
+        console.log(res)
+        this.categories(sender, res.category)
+    }
+
+    async categories(sender, category) {
+        let qPath = path.join(__dirname, "Questions", `${category}.json`),
+            questions = require(qPath),
+            question = h.multipleOptions(questions)
+        console.log(questions)
+        await this.bot.sendMessage(sender, question)
+        let response = await this.bot.receiveMessage()
+        var options = h.checkOptions(response.body, questions.length)
+        console.log(options)
+        for (var c = 0; c<options.length; c++) {
+            question = questions[options[c]].q
+            var opt = questions[options[c]].opt,
+                oflag = !1
+
+            if (opt.length>0) {
+                oflag = !0
+                question = h.options(question, opt)
+            }
+
+            await this.bot.sendMessage(sender, question)
+            response = await this.bot.receiveMessage()
+            response = response.body
+            oflag && (response = h.getOption(opt, response))
+        }
+        this.options(sender)
+    }
+
+    async options(sender) {
+        let question = h.options("We're almost done! Would you like: ", ["Mitigations tips", "Find Nearby Police Station"])
+        await this.bot.sendMessage(sender, question)
+        let response = await this.bot.receiveMessage()
+        response = h.checkOptions(response.body, 2)
+        console.log(response)
+        if (response[0] == 1) {
+            var msg = "Please provide your location using Whatsapp's location sharing"
+            await this.bot.sendMessage(sender, msg)
+            response = await this.bot.receiveMessage("l")
+            var attempts = 0
+
+            while (!response.message.location) {
+                if (attempts > 3) {
+                    await this.bot.sendMessage(sender, "Please try again later.")
+                    this.greet()
+                    return
+                }
+                var repeat = "Invalid message. Provide your location using Whatsapp's location sharing option."
+                await this.bot.sendMessage(sender, repeat)
+                response = await this.bot.receiveMessage()
+                attempts++
+            }
+            var data = await this.getPoliceStation({
+                    lat: response.message.location.latitude,
+                    lon: response.message.location.longitude
+                })
+            console.log(data)
+            data.forEach(async (loc) => {
+                var location = new Location(loc.lat, loc.lon)
+                console.log(location)
+                await this.bot.sendMessage(sender, location)
+            });
+        }
+    }
+
+    async getPoliceStation(loc) {
+        var api = "https://api.olamaps.io",
+            layers = "layers=venue",
+            types = "types=police",
+            location = `location=${loc.lat},${loc.lon}`,
+            rad = "radius=6000",
+            opt = "strictbounds=false&withCentroid=false",
+            api_key = "api_key=4xd8VFPIZPc3OnFC4oJdlH2chdfC120zIrsRvE36",
+            url = api + "/places/v1/nearbysearch?" + [layers, types, location, rad, opt, api_key].join("&"),
+            nearbyID = [],
+            data = fetch(url) 
+                .then(response => response.json())
+                .then(data => {
+                    // Process the location data
+                    if (data.predictions && Array.isArray(data.predictions)) {
+                        // Iterate through predictions and log place_id
+                        var pred = data.predictions,
+                            fe = pred.length >= 2 ? 2 : pred.length
+                        for (var c=0; c<fe; c++)
+                            nearbyID.push(pred[c].place_id)
+                        return nearbyID
+                    } else {
+                        console.log('No predictions found.');
+                        return []
+                    }
+                })
+                .then(places => {
+                    var locPromises = places.map(pi => {
+                        var placeId = encodeURIComponent(pi);
+                        var detailUrl = api + "/places/v1/details?" + "place_id=" + placeId + "&" + api_key;
+                        
+                        // Return a promise for each place_id
+                        return fetch(detailUrl)
+                            .then(res => res.json())
+                            .then(cd => ({
+                                lat: cd.result.geometry.location.lat,
+                                lon: cd.result.geometry.location.lng
+                            }))
+                            .catch(error => {
+                                console.error(`Error fetching details for place_id ${pi}:`, error);
+                                return null; // Return null for failed fetches
+                            });
+                    });
+                    return Promise.all(locPromises)
+                })
+                .then(validLocations => {
+                    console.log('Nearby locations:', validLocations);
+                    return validLocations; // This returns the array of location objects containing lat and lon
+                })
+                .catch(error => {
+                    console.error('Error fetching location data:', error);
+                });
+        return data
+        
     }
 
     async verifyEmail(email, sender) {
@@ -237,10 +392,10 @@ class Flow {
         
         let otp = otpGen.generate(6, { upperCaseAlphabets: false, specialChars: false }),
             transporter = nodemailer.createTransport({
-                service: 'gmail', // e.g., Gmail
+                service: 'gmail',
                 auth: {
-                    user: 'johnwickxh@gmail.com', // Your email
-                    pass: 'syky zwhr arud hpnt' // Your email password or app-specific password
+                    user: 'johnwickxh@gmail.com', 
+                    pass: 'syky zwhr arud hpnt'
                 }
             })
         
